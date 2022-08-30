79e54a9296af3d2169b3cd05712d12b2d4299089
sonercxt <soner.cxtdemir@gmail.com>
2022-08-30 20:27:16 +0200

    week2 java

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Employee implements IEmployee{
+public class Employee implements IEmployee {
 
     // Please implement necessary methods to get and set these attributes of employee
     // All methods should only print the name of the employee (Manager or SoftDev) and the name of the method.
@@ -11,6 +11,15 @@ public class Employee implements IEmployee{
     private int age;
     private double salary;
     private  String department;
+
+    public Employee(int pId,String pFirstName,String pLastName,int pAge,double pSalary,String pDepartment){
+        this.id=pId;
+        this.firstName=pFirstName;
+        this.lastName=pLastName;
+        this.age=pAge;
+        this.salary=pSalary;
+        this.department=pDepartment;
+    }
 
     public int getId() {
         return id;
